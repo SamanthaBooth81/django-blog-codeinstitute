793627ee783b5faa4c8974b78d565d4b4d4b03bd
SamanthaBooth81 <samanthabooth09@live.co.uk>
2021-12-20 21:48:27 +0000

    add admin functionality. Manage posts, Create drafts and  Approve comments

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,31 @@
 from django.contrib import admin
+from .models import Post, Comment
+from django_summernote.admin import SummernoteModelAdmin
 
-# Register your models here.
+
+@admin.register(Post)
+class PostAdmin(SummernoteModelAdmin):
+
+    list_display = ('title', 'slug', 'status', 'created_on')
+    search_fields = ['title', 'content']
+    # we want to use summer note for our content django text field
+    prepopulated_fields = {'slug': ('title',)}
+    # add a filter to admin page to search by satus
+    # (draft/posted) or when created
+    list_filter = ('status', 'created_on')
+    summernote_fields = ('content')
+
+# admin.site.register(Post) - this method
+# only allows 2 arguments to be passed to it
+# so I have added the @admin decorator aove the PostAdmin class
+
+
+@admin.register(Comment)
+class commentAdmin(admin.ModelAdmin):
+    list_display = ('name', 'body', 'post', 'created_on', 'approved')
+    list_filter = ('approved', 'created_on')
+    search_fields = ['name', 'email', 'body']
+    actions = ['approve_comments']
+
+    def approve_comments(self, request, queryset):
+        queryset.update(approved=True)
--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,49 @@
 from django.db import models
+# imports the user model from Django
+from django.contrib.auth.models import User
+# imports Cloudinary field for the feature image
+from cloudinary.models import CloudinaryField
 
-# Create your models here.
+# create a tuple for the blog post status
+STATUS = ((0, 'Draft'), (1, 'Published'))
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=200, unique=True)
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='blog_posts')
+    updated_on = models.DateTimeField(auto_now=True)
+    content = models.TextField()
+    featured_image = CloudinaryField('image', default='placeholder')
+    excerpt = models.TextField(blank=True)
+    created_on = models.DateTimeField(auto_now_add=True)
+    status = models.IntegerField(choices=STATUS, default=0)
+    likes = models.ManyToManyField(User, related_name='blog_likes', blank=True)
+
+    class Meta:
+        # the '-' creates a descending order for the post
+        ordering = ['-created_on']
+
+    def __str__(self):
+        return self.title
+
+    def number_of_likes(self):
+        return self.likes.count()
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='comments')
+    name = models.CharField(max_length=80)
+    email = models.EmailField()
+    body = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    approved = models.BooleanField(default=False)
+
+    class Meta:
+        # posts comments in ascending order for the post
+        ordering = ['created_on']
+
+    def __str__(self):
+        return f"comment {self.body} by {self.name}"
